9cf4470513ea4572b5745a1dba80e801c38600c1
Panida Wiriyachaiporn <neufii.s@gmail.com>
2017-12-14 16:52:14 +0700

    edit log

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def bot():
     print("TEXT\t"+text)
 
     response = chatbot.get_response(text)
-    print("RESPONSE" + response)
+    print(response)
     # ตอบข้อความ "นี่คือรูปแบบข้อความที่รับส่ง" กลับไป
     
     # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไปมา (แบบ json)
