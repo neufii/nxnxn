82bc21c0031e2c1f82909a17af5e8f3ee38760c9
Panida Wiriyachaiporn <neufii.s@gmail.com>
2017-12-14 16:16:37 +0700

    del decode

--- app.py
+++ app.py
@@ -63,8 +63,7 @@ def bot():
 
     print("TEXT\t"+text)
 
-    decoded = text.decode("utf-8")
-    response = chatbot.get_response(decoded)
+    response = chatbot.get_response(text)
     # ตอบข้อความ "นี่คือรูปแบบข้อความที่รับส่ง" กลับไป
     
     # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไปมา (แบบ json)
